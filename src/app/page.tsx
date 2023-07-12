93d980ed7942b125ee0ada3548fc9042f6e46d87
Greg G <garabedium@users.noreply.github.com>
2023-07-11 21:11:54 -0400

    feat: adds Pixabay api fetch query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 export default function Home() {
   const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState('');
+
+  const apiBase = "https://pixabay.com/api/?";
+  const apiKey = "key={TODO:REPLACE_WITH_ENV_VAR}";
+
+  const queryApi = useCallback(async() => {
+    const apiUrl = `${apiBase}${apiKey}&q=${query}`;
+    const options = { method: 'GET' };
+    try {
+      const response = await fetch(apiUrl, options);
+      const { hits } = await response.json();
+      setResults(hits);
+    } catch (error) {
+      setError('Oops. That search failed.')
+    }
+
+  }, [query])  
 
   const handleQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setQuery(value);
   }
+
+  useEffect(() => {
+    if (query.length){
+      queryApi();
+    }
+  }, [query, queryApi]);
 
   return (
     <main>
@@ -20,6 +44,8 @@ export default function Home() {
           value={query}
           onChange={handleQuery}
         />
+
+        {error}
       </div>
     </main>
   )
