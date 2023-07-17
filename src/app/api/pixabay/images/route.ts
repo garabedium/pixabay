b985f9e000acbf2ee6665004330cc7120a2c9db5
Greg G <garabedium@users.noreply.github.com>
2023-07-17 09:59:50 -0400

    feat: add api images route

--- src/app/api/pixabay/images/route.ts
+++ src/app/api/pixabay/images/route.ts
@@ -1,10 +1,24 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { 
+  pixabayApiBase as apiBase,
+  pixabayApiKey as apiKey,
+ } from "@/utils/constants.util";
 
-export async function GET(request: NextResponse){
-  // const url = JSON.parse(request.url);
-  // console.log(url)
-  console.log(request.nextUrl.searchParams)
-  console.log(process.env.PIXABAY_API_KEY)
-  console.log(process.env.NODE_ENV)
-  return NextResponse.json({ message: 'lorem'}, { status: 200 })
+export async function GET(req: NextRequest){
+  const query = req.nextUrl.searchParams.get('query');
+  const apiUrl = `${apiBase}${apiKey}&q=${query}`;
+  const options = { method: 'GET' };
+
+  try {
+    const response = await fetch(apiUrl, options);
+    const { hits } = await response.json();
+
+    return NextResponse.json(
+      { data: hits}, { status: 200 }
+    )   
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Oops...'}, { status: 500 }
+    )
+  }
 }
